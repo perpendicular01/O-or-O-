32462ff7bf1c951e8ccf89434dd89c009deee5d4
saif <kh.s.k.rohan@gmail.com>
2025-05-30 22:23:14 +0600

    update dashboard

--- Client Version/src/Layout/Dashboard.jsx
+++ Client Version/src/Layout/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BsCalendarHeartFill } from 'react-icons/bs';
 import { FaCalendarAlt, FaShoppingBag, FaShoppingCart, FaUsers, FaUtensils } from 'react-icons/fa';
 import { IoMdHome } from 'react-icons/io';
@@ -13,6 +13,11 @@ import useAdmin from '../hooks/useAdmin';
 import useVolenteer from '../hooks/useVolenteer';
 import useDonor from '../hooks/useDonor';
 // import useAdmin from '../hooks/useAdmin';
+import { FaArrowCircleRight } from "react-icons/fa";
+
+
+import Drawer from 'react-modern-drawer'
+import 'react-modern-drawer/dist/index.css'
 
 const Dashboard = () => {
     const { logOut } = useAuth();
@@ -34,13 +39,20 @@ const Dashboard = () => {
         catch { /* empty */ }
     }
 
+    const [isOpen, setIsOpen] = useState(false);
+
+
+    const toggleDrawer = () => {
+        setIsOpen(!isOpen);
+    }
+
 
 
     return (
         <div>
             <div className='flex'>
                 {/* Sidebar Area */}
-                <div className='relative lg:pl-5 w-[22%] min-h-screen   text-black'>
+                <div className='relative lg:pl-5 w-[22%] min-h-screen hidden md:block   text-black'>
                     {/* <h2 className='mt-5 uppercase text-3xl mr-14 text-center font-bold'> dish</h2>
                     <h2 className='uppercase  text-3xl ml-10 text-center font-bold mb-7'> Dash</h2> */}
                     <div className='flex flex-col justify-center items-center mt-4 mb-8'>
@@ -337,6 +349,310 @@ const Dashboard = () => {
                     </div>
 
                 </div>
+
+
+
+                {/* for mbile device */}
+                <div onClick={toggleDrawer} className='md:hidden text-redd z-10 ml-2 -mr-3 mt-36 '> <FaArrowCircleRight className='text-3xl  ' /> </div>
+                <Drawer style={{backgroundColor: '', width: ''}} open={isOpen} onClose={toggleDrawer} direction="left">
+                        <div>
+                            {/* <h2 className='mt-5 uppercase text-3xl mr-14 text-center font-bold'> dish</h2>
+                            <h2 className='uppercase  text-3xl ml-10 text-center font-bold mb-7'> Dash</h2> */}
+                            <div className='flex flex-col justify-center items-center mt-4 mb-8'>
+                                <img src={logo} className="w-10 h-10" alt="" />
+                                <h2 className='mt-1 uppercase text-md text-center font-bold text-redd '> O+orO- </h2>
+
+                            </div>
+
+                            
+
+                            {/* for donor */}
+                            {
+                                isDonor &&
+                                <ul className='menu p-4  lg:space-y-2'>
+
+
+                                    <li>
+                                        <NavLink
+                                            to="/dashboard"
+                                            end
+                                            className={({ isActive }) =>
+                                                isActive
+                                                    ? 'bg-redd text-white rounded-lg'
+                                                    : 'hover:bg-redd/50 rounded-lg'
+                                            }
+                                        >
+                                            <div className='flex items-center justify-center gap-2'>
+                                                <div className='text-2xl'>
+                                                    <IoMdHome />
+                                                </div>
+                                                <h2 className=' text-md'> DashBoard </h2>
+                                            </div>
+                                        </NavLink>
+                                    </li>
+
+                                    <li>
+                                        <NavLink
+                                            to="/dashboard/profile"
+                                            className={({ isActive }) =>
+                                                isActive
+                                                    ? 'bg-redd text-white rounded-lg'
+                                                    : 'hover:bg-redd/50 rounded-lg'
+                                            }
+                                        >
+                                            <div className='flex items-center justify-center gap-2'>
+                                                <div className='text-2xl'>
+                                                    <FaUtensils />
+                                                </div>
+                                                <h2 className=' text-md'> Profile </h2>
+                                            </div>
+                                        </NavLink>
+                                    </li>
+
+                                    <li>
+                                        <NavLink
+                                            to="/dashboard/my-donation-requests"
+                                            className={({ isActive }) =>
+                                                isActive
+                                                    ? 'bg-redd text-white rounded-lg'
+                                                    : 'hover:bg-redd/50 rounded-lg'
+                                            }
+                                        >
+                                            <div className='flex items-center justify-center gap-2'>
+                                                <div className='text-2xl'>
+                                                    <TfiMenuAlt />
+                                                </div>
+                                                <h2 className='uppercase text-md'> My Request</h2>
+                                            </div>
+                                        </NavLink>
+                                    </li>
+
+
+                                    <li>
+                                        <NavLink
+                                            to="/dashboard/create-donation-request"
+                                            className={({ isActive }) =>
+                                                isActive
+                                                    ? 'bg-redd text-white rounded-lg'
+                                                    : 'hover:bg-redd/50 rounded-lg'
+                                            }
+                                        >
+                                            <div className='flex items-center justify-center gap-2'>
+                                                <div className='text-2xl'>
+                                                    <BiSolidFoodMenu />
+                                                </div>
+                                                <h2 className='uppercase text-md'> Create Request </h2>
+                                            </div>
+                                        </NavLink>
+                                    </li>
+
+                                </ul>
+                            }
+
+
+                            {/* for amdin */}
+                            {
+                                isAdmin &&
+                                <ul className='menu p-4  lg:space-y-2'>
+
+
+                                    <li>
+                                        <NavLink
+                                            to="/dashboard"
+                                            end
+                                            className={({ isActive }) =>
+                                                isActive
+                                                    ? 'bg-redd text-white rounded-lg'
+                                                    : 'hover:bg-redd/50 rounded-lg'
+                                            }
+                                        >
+                                            <div className='flex items-center justify-center gap-2'>
+                                                <div className='text-2xl'>
+                                                    <IoMdHome />
+                                                </div>
+                                                <h2 className=' text-md'> DashBoard </h2>
+                                            </div>
+                                        </NavLink>
+                                    </li>
+
+                                    <li>
+                                        <NavLink
+                                            to="/dashboard/profile"
+                                            className={({ isActive }) =>
+                                                isActive
+                                                    ? 'bg-redd text-white rounded-lg'
+                                                    : 'hover:bg-redd/50 rounded-lg'
+                                            }
+                                        >
+                                            <div className='flex items-center justify-center gap-2'>
+                                                <div className='text-2xl'>
+                                                    <FaUtensils />
+                                                </div>
+                                                <h2 className=' text-md'> Profile </h2>
+                                            </div>
+                                        </NavLink>
+                                    </li>
+
+                                    <li>
+                                        <NavLink
+                                            to="/dashboard/all-users"
+                                            className={({ isActive }) =>
+                                                isActive
+                                                    ? 'bg-redd text-white rounded-lg'
+                                                    : 'hover:bg-redd/50 rounded-lg'
+                                            }
+                                        >
+                                            <div className='flex items-center justify-center gap-2'>
+                                                <div className='text-2xl'>
+                                                    <FaUtensils />
+                                                </div>
+                                                <h2 className=' text-md'> All Users </h2>
+                                            </div>
+                                        </NavLink>
+                                    </li>
+
+                                    <li>
+                                        <NavLink
+                                            to="/dashboard/all-blood-donation-request"
+                                            className={({ isActive }) =>
+                                                isActive
+                                                    ? 'bg-redd text-white rounded-lg'
+                                                    : 'hover:bg-redd/50 rounded-lg'
+                                            }
+                                        >
+                                            <div className='flex items-center justify-center gap-2'>
+                                                <div className='text-2xl'>
+                                                    <TfiMenuAlt />
+                                                </div>
+                                                <h2 className='uppercase text-md'> Blood Request </h2>
+                                            </div>
+                                        </NavLink>
+                                    </li>
+
+
+                                    <li>
+                                        <NavLink
+                                            to="/dashboard/content-management"
+                                            className={({ isActive }) =>
+                                                isActive
+                                                    ? 'bg-redd text-white rounded-lg'
+                                                    : 'hover:bg-redd/50 rounded-lg'
+                                            }
+                                        >
+                                            <div className='flex items-center justify-center gap-2'>
+                                                <div className='text-2xl'>
+                                                    <BiSolidFoodMenu />
+                                                </div>
+                                                <h2 className='uppercase text-md'> Content Management </h2>
+                                            </div>
+                                        </NavLink>
+                                    </li>
+
+                                </ul>
+                            }
+
+                            {/* for volenteer */}
+                            {
+                                isVolenteer &&
+                                <ul className='menu p-4  lg:space-y-2'>
+
+
+                                    <li>
+                                        <NavLink
+                                            to="/dashboard"
+                                            end
+                                            className={({ isActive }) =>
+                                                isActive
+                                                    ? 'bg-redd text-white rounded-lg'
+                                                    : 'hover:bg-redd/50 rounded-lg'
+                                            }
+                                        >
+                                            <div className='flex items-center justify-center gap-2'>
+                                                <div className='text-2xl'>
+                                                    <IoMdHome />
+                                                </div>
+                                                <h2 className=' text-md'> DashBoard </h2>
+                                            </div>
+                                        </NavLink>
+                                    </li>
+
+                                    <li>
+                                        <NavLink
+                                            to="/dashboard/profile"
+                                            className={({ isActive }) =>
+                                                isActive
+                                                    ? 'bg-redd text-white rounded-lg'
+                                                    : 'hover:bg-redd/50 rounded-lg'
+                                            }
+                                        >
+                                            <div className='flex items-center justify-center gap-2'>
+                                                <div className='text-2xl'>
+                                                    <FaUtensils />
+                                                </div>
+                                                <h2 className=' text-md'> Profile </h2>
+                                            </div>
+                                        </NavLink>
+                                    </li>
+
+                                    <li>
+                                        <NavLink
+                                            to="/dashboard/all-blood-donation-request"
+                                            className={({ isActive }) =>
+                                                isActive
+                                                    ? 'bg-redd text-white rounded-lg'
+                                                    : 'hover:bg-redd/50 rounded-lg'
+                                            }
+                                        >
+                                            <div className='flex items-center justify-center gap-2'>
+                                                <div className='text-2xl'>
+                                                    <TfiMenuAlt />
+                                                </div>
+                                                <h2 className='uppercase text-md'> Blood Request </h2>
+                                            </div>
+                                        </NavLink>
+                                    </li>
+
+
+                                    <li>
+                                        <NavLink
+                                            to="/dashboard/content-management"
+                                            className={({ isActive }) =>
+                                                isActive
+                                                    ? 'bg-redd text-white rounded-lg'
+                                                    : 'hover:bg-redd/50 rounded-lg'
+                                            }
+                                        >
+                                            <div className='flex items-center justify-center gap-2'>
+                                                <div className='text-2xl'>
+                                                    <BiSolidFoodMenu />
+                                                </div>
+                                                <h2 className='uppercase text-md'> Content Management </h2>
+                                            </div>
+                                        </NavLink>
+                                    </li>
+
+                                </ul>
+                            }
+
+
+
+                            <div className="absolute bottom-10 lg:pl-6  p-4">
+                                <hr className="border-t-2 border-gray-400 my-4 w-full" />
+
+
+                                <NavLink
+                                    to="/logout"
+                                    className=' text-black rounded-lg hover:bg-blue-100 absolute bottom-0'
+                                >
+                                    <button
+                                        onClick={handleSignOut}
+                                        className='btn px-16 rounded-xl border-redd flex items-center gap-2'>
+                                        <h2 className='uppercase text-md'> Logout </h2>
+                                    </button>
+                                </NavLink>
+                            </div>
+                        </div>
+                    </Drawer>
 
                 {/* Main Content Area */}
                 <div className='flex-1 bg-gray-50'>
